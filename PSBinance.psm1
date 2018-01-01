66d6c5c116907c3caf75e3c75586401a73555452
Jorgen <jwdegier@wxs.nl>
2018-01-02 00:29:35 +0100

    Eerste 15 cmdlets

--- PSBinance.psm1
+++ PSBinance.psm1
@@ -525,3 +525,556 @@ function Get-BinOrderBook
     {
     }
 }
+
+<#
+.Synopsis
+   Get Binance Trades
+.DESCRIPTION
+   Get recent trades for selected symbol.
+.EXAMPLE
+   Get-BinTrades
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Get-BinTrades
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Asset symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol,
+        # Limit resultset
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateRange(1,500)]
+        [int16]
+        $limit = 500
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $message = "?symbol=$symbol&limit=$limit"
+
+            $resource = "https://api.binance.com/api/v1/trades$message"
+            $trades = Invoke-RestMethod -Method Get -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $trades
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Place Sell Order
+.DESCRIPTION
+   Set a sell order for selected symbol.
+.EXAMPLE
+   Set-BinSellOrder
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Set-BinSellOrder
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Asset symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol,
+        # Quantity
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateNotNull()]
+        [int16]
+        $quantity,
+        # price
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateNotNull()]
+        [int16]
+        $price
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $currenttime = ([DateTimeOffset](Get-Date)).ToUnixTimeMilliseconds()
+            $message = "symbol=$symbol&side=SELL&type=LIMIT&timestamp=$($currenttime)"
+
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+            $signature = -join ( $signature | ForEach-Object{"{0:x2}"-f $_})
+
+            $message = "?$message&signature=$signature"
+            $message = "?symbol=$symbol&limit=$limit"
+
+            $resource = "https://api.binance.com/api/v1/trades$message"
+            $trades = #Invoke-RestMethod -Method Get -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $trades
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Place Buy Order
+.DESCRIPTION
+   Set a sell order for selected symbol.
+.EXAMPLE
+   Set-BinBuyOrder
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Set-BinBuyOrder
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Asset symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol,
+        # Quantity
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateNotNull()]
+        $quantity,
+        # price
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=2)]
+        [ValidateNotNull()]
+        $price
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $currenttime = ([DateTimeOffset](Get-Date)).ToUnixTimeMilliseconds()
+            $message = "symbol=$symbol&side=BUY&type=LIMIT&quantity=$quantity&price=$price&&timestamp=$($currenttime)"
+
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+            $signature = -join ( $signature | ForEach-Object{"{0:x2}"-f $_})
+
+            $message = "?$message&signature=$signature"
+
+            $resource = "https://api.binance.com/api/v1/trades$message"
+            $trades = Invoke-RestMethod -Method Post -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $trades
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Get open Ordera
+.DESCRIPTION
+   Get the open orders for selected symbol.
+.EXAMPLE
+   Get-BinOpenOrder
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Get-BinOpenOrder
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String[]])]
+    Param
+    (
+        # Trade symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $currenttime = ([DateTimeOffset](Get-Date)).ToUnixTimeMilliseconds()
+            $message = "symbol=$symbol&timestamp=$($currenttime)"
+
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+            $signature = -join ( $signature | ForEach-Object{"{0:x2}"-f $_})
+
+            $message = "?$message&signature=$signature"
+
+            $resource = "https://api.binance.com/api/v3/openOrders$message"
+            $openorders = Invoke-RestMethod -Method Get -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $openorders
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Remove open Order
+.DESCRIPTION
+   Remove  open orders for selected symbol.
+.EXAMPLE
+   Get-BinOpenOrder XVGETH | %{ Remove-BinOpenOrder $_.symbol $_.orderid}
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Remove-BinOpenOrder
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Trade symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$true,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol,
+        # Trade symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$true,
+                   Position=1)]
+        [ValidateNotNullOrEmpty()]
+        $orderID
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $currenttime = ([DateTimeOffset](Get-Date)).ToUnixTimeMilliseconds()
+            $message = "symbol=$symbol&orderId=$orderID&timestamp=$($currenttime)"
+
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+            $signature = -join ( $signature | ForEach-Object{"{0:x2}"-f $_})
+
+            $message = "?$message&signature=$signature"
+
+            $resource = "https://api.binance.com/api/v3/order$message"
+
+            $removeOrder = Invoke-RestMethod -Method Delete -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $removeOrder
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Query order
+.DESCRIPTION
+   Check an order's status.
+.EXAMPLE
+   Get-BinOrderStatus
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Get-BinOrderStatus
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String[]])]
+    Param
+    (
+        # Trade symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol,
+        # Order ID
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=1)]
+        [ValidateNotNullOrEmpty()]
+        $OrderID
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $currenttime = ([DateTimeOffset](Get-Date)).ToUnixTimeMilliseconds()
+            $message = "symbol=$symbol&orderId=$orderID&timestamp=$($currenttime)"
+
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+            $signature = -join ( $signature | ForEach-Object{"{0:x2}"-f $_})
+
+            $message = "?$message&signature=$signature"
+
+            $resource = "https://api.binance.com/api/v3/order$message"
+            $openorders = Invoke-RestMethod -Method Get -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $openorders
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Place Buy Order
+.DESCRIPTION
+   Set a sell order for selected symbol.
+.EXAMPLE
+   Set-BinBuyOrder
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   Binance Exchange
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+function Test-BinBuyOrder
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                   PositionalBinding=$false,
+                   HelpUri = 'http://www.microsoft.com/',
+                   ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Asset symbol
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [ValidateNotNullOrEmpty()]
+        $Symbol,
+        # Quantity
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=1)]
+        [ValidateNotNull()]
+        $quantity,
+        # price
+        [Parameter(Mandatory=$false, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=2)]
+        [ValidateNotNull()]
+        $price
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+        {
+            $currenttime = ([DateTimeOffset](Get-Date)).ToUnixTimeMilliseconds()
+            $message = "symbol=$symbol&side=BUY&type=LIMIT&timeInForce=GTC&quantity=$quantity&price=$price&&timestamp=$($currenttime)"
+
+            $hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+            $hmacsha.key = [Text.Encoding]::ASCII.GetBytes($secret)
+
+            $signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($message))
+            $signature = -join ( $signature | ForEach-Object{"{0:x2}"-f $_})
+
+            $message = "?$message&signature=$signature"
+
+            $resource = "https://api.binance.com/api/v3/order/test$message"
+            $trades = Invoke-RestMethod -Method Post -Uri "$resource" -Header @{ "X-MBX-APIKEY" = $apiKey }
+            $trades
+        }
+    }
+    End
+    {
+    }
+}
+
+
+<#
+
+remove-module psbinance ; import-module .\psbinance.psm1
+
+#>
